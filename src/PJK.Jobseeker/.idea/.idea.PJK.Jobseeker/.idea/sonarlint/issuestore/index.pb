9d313fba03c8129177a5e281525ec6c460ee26d5
paul.kane <paul.kane@defra.onmicrosoft.com>
2025-01-14 11:31:11 +0000

    Added folder structure to solution

---
+++ src/PJK.Jobseeker/.idea/.idea.PJK.Jobseeker/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+^
+.PJK.Jobseeker.Domain/Common/AuditableEntity.cs,7/0/7083034e8fdefba74d5b24517f8266f2298fdfc1
+s
+CPJK.Jobseeker.Application/Contracts/Persistence/IAsyncRepository.cs,5/3/53ed11aacc0b6008e4c0a3870b0ec16c3cf33274
+s
+CPJK.Jobseeker.Application/Contracts/Persistence/IEventRepository.cs,e/a/eaa6216ae5d9efe2e0b97f68bce7fdf6ac4bb44f
---
+++ src/PJK.Jobseeker/PJK.Jobseeker.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace PJK.Jobseeker.Application.Contracts.Persistence;
+
+public interface IAsyncRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<IReadOnlyList<T>> GetPagedReponseAsync(int page, int size);
+}
